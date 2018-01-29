6a8c8b1d8567f122c6c0bd5b6e1ece0cf0023bf1
Joshua Holloway <josh.holloway@live.co.uk>
2018-01-29 16:30:38 +0000

    balance defined and rspec for it working

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,16 @@ require 'oystercard'
 
 describe Oystercard do
 
+  describe "#new card" do
+
+    before do
+      @oystercard = Oystercard.new
+    end
+
+    it 'should have a default balance of 0' do
+      expect(@oystercard.balance).to eq 0
+    end
+  end
 end
 
 
